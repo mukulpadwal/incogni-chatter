66539f2debc966f14fee24d2576458d46b343c1e
mukulpadwal <mukulpadwal.me@gmail.com>
2024-04-11 16:40:47 +0530

    FEAT : db-connection

---
+++ src/lib/dbConnect.ts
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+
+type ConnectionObject = {
+    isConnected?: number
+}
+
+const connection: ConnectionObject = {};
+
+export default async function connectToDb(): Promise<void> {
+    // Check if database is already connected or not
+    if (connection.isConnected) {
+        console.log(`Already connected to database...`);
+        return;
+    }
+
+    try {
+        const dbConnection = await mongoose.connect(process.env.MONGODB_URI!)
+
+        console.log(dbConnection);
+        console.log(dbConnection.connections);
+        
+
+        connection.isConnected = dbConnection.connections[0].readyState;
+
+        console.log(`DB connected successfully...`);
+        
+
+    } catch (error: any) {
+        console.log(`Database connection failed : ERROR : ${error.message}`);
+        process.exit(1);
+    }
+}
