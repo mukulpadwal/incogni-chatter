c4418a59afb94ea18788e2e3022033ca9fe66317
mukulpadwal <mukulpadwal.me@gmail.com>
2024-04-27 13:51:47 +0530

    ADD : verify-account-page

---
+++ src/app/verify/[username]/page.tsx
@@ -0,0 +1,117 @@
+"use client";
+
+import { useToast } from "@/components/ui/use-toast";
+import ApiResponse from "@/types/ApiResponse";
+import verifySchema from "@/schemas/verify.schema";
+import { zodResolver } from "@hookform/resolvers/zod";
+import axios, { AxiosError } from "axios";
+import { useParams, useRouter } from "next/navigation";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Button } from "@/components/ui/button";
+import {
+  InputOTP,
+  InputOTPGroup,
+  InputOTPSlot,
+} from "@/components/ui/input-otp";
+
+const VeriyAccountPage = () => {
+  const router = useRouter();
+  const params = useParams<{ username: string }>();
+  const { toast } = useToast();
+
+  const form = useForm<z.infer<typeof verifySchema>>({
+    resolver: zodResolver(verifySchema),
+    defaultValues: {
+      verifyOtp: "",
+    },
+  });
+
+  const onSubmit = async (data: z.infer<typeof verifySchema>) => {
+    try {
+      const response = await axios.post("/api/verify-otp", {
+        username: params.username,
+        otp: data.verifyOtp,
+      });
+
+      if (response.data.success) {
+        toast({
+          title: "Success",
+          description: response.data.message,
+        });
+
+        router.replace("/sign-in");
+      }
+    } catch (error) {
+      const axiosError = error as AxiosError<ApiResponse>;
+      let errorMessage = axiosError.response?.data.message;
+      toast({
+        title: "OTP Verification failed",
+        description: errorMessage,
+        variant: "default",
+      });
+    } finally {
+    }
+  };
+
+  return (
+    <div className="flex justify-center items-center min-h-screen bg-gray-100">
+      <div className="w-full max-w-md p-8 space-y-8 bg-white rounded-lg shadow-md">
+        <div className="text-center">
+          <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl mb-6">
+            Verify your Incogni Chatter Account
+          </h1>
+          <p className="mb-4">
+            Enter the verification otp sent to your account.
+          </p>
+        </div>
+
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+            <FormField
+              control={form.control}
+              name="verifyOtp"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>One-Time Password</FormLabel>
+                  <FormControl>
+                    <InputOTP maxLength={6} {...field}>
+                      <InputOTPGroup>
+                        <InputOTPSlot index={0} />
+                        <InputOTPSlot index={1} />
+                        <InputOTPSlot index={2} />
+                        <InputOTPSlot index={3} />
+                        <InputOTPSlot index={4} />
+                        <InputOTPSlot index={5} />
+                      </InputOTPGroup>
+                    </InputOTP>
+                  </FormControl>
+                  <FormDescription>
+                    Please enter the one-time password sent to your email.
+                  </FormDescription>
+
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button className="w-full" type="submit">
+              Submit
+            </Button>
+          </form>
+        </Form>
+      </div>
+    </div>
+  );
+};
+
+
+export default VeriyAccountPage;
